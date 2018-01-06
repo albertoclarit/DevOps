360358946e750ed7241ba9d7c7aeb7cc4049a19d
Albert Oclarit <albertoclarit@gmail.com>
2018-01-06 17:00:46 +0800

    First Commit

--- settingk8.sh
+++ settingk8.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# curl -sS https://get.fabric8.io/download.sh | bash
+# curl -sS https://raw.githubusercontent.com/albertoclarit/DevOps/master/settingk8.sh | bash
 
 
 apt-get update
@@ -13,4 +13,22 @@ deb http://apt.kubernetes.io/ kubernetes-xenial main
 EOF
 apt-get update
 apt-get install -y kubelet kubeadm kubectl kubernetes-cni
+
+
+sudo add-apt-repository ppa:gophers/archive
+sudo apt update
+sudo apt-get install golang-1.9-go
+wget https://github.com/kubernetes-incubator/cri-tools/archive/v1.0.0-alpha.0.zip
+apt-get install -y unzip
+unzip v1.0.0-alpha.0.zip -d xx
+
+echo "export PATH=\"\$PATH:/usr/lib/go-1.9/bin\"" >> .profile
+echo "export PATH=\"\$PATH:/root/xx/cri-tools-1.0.0-alpha.0/_output/bin\"" >> .profile
+source ~/.profile
+cd xx/cri-tools-1.0.0-alpha.0
+make
+cd ~
+
+
+
 
