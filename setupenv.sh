b35539cf51d4ce22b154a6968665724e776756ae
Albert Oclarit <albertoclarit@gmail.com>
2018-01-06 17:22:04 +0800

    First Commit

---
+++ setupenv.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# curl -sS https://raw.githubusercontent.com/albertoclarit/DevOps/master/setupenv.sh | bash
+
+mkdir -p $HOME/.kube
+sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+sudo chown $(id -u):$(id -g) $HOME/.kube/config
