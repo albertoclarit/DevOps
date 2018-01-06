0ef914b800c7dacc5aa2de7bea55920474c350cc
Albert Oclarit <albertoclarit@gmail.com>
2018-01-06 17:35:31 +0800

    First Commit

--- setupenv.sh
+++ setupenv.sh
@@ -6,3 +6,6 @@ sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 sudo chown $(id -u):$(id -g) $HOME/.kube/config
 
 cd ~
+
+echo "source <(kubectl completion bash)" >> ~/.profile
+source ~/.profile
