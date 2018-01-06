1d8298c705015b9bff39d0b6f6cab27dbe30af0b
Albert Oclarit <albertoclarit@gmail.com>
2018-01-06 16:50:16 +0800

    First Commit

---
+++ settingk8.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# curl -sS https://get.fabric8.io/download.sh | bash
+
+
+apt-get update
+apt-get install -y docker.io
+
+
+apt-get update && apt-get install -y apt-transport-https
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
+cat <<EOF >/etc/apt/sources.list.d/kubernetes.list
+deb http://apt.kubernetes.io/ kubernetes-xenial main
+EOF
+apt-get update
+apt-get install -y kubelet kubeadm kubectl kubernetes-cni
+
