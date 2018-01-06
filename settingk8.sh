3bb41b5597ceab560bad525723fee3fe8023133b
Albert Oclarit <albertoclarit@gmail.com>
2018-01-06 17:07:54 +0800

    First Commit

--- settingk8.sh
+++ settingk8.sh
@@ -2,22 +2,7 @@
 # curl -sS https://raw.githubusercontent.com/albertoclarit/DevOps/master/settingk8.sh | bash
 
 
-apt-get update
-apt-get install -y docker.io
-
-
-apt-get update && apt-get install -y apt-transport-https
-curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
-cat <<EOF >/etc/apt/sources.list.d/kubernetes.list
-deb http://apt.kubernetes.io/ kubernetes-xenial main
-EOF
-apt-get update
-apt-get install -y kubelet kubeadm kubectl kubernetes-cni
-
-
-sudo add-apt-repository ppa:gophers/archive
-sudo apt update
-sudo apt-get install golang-1.9-go
+sudo apt-get install -y golang-1.9-go
 wget https://github.com/kubernetes-incubator/cri-tools/archive/v1.0.0-alpha.0.zip
 apt-get install -y unzip
 unzip v1.0.0-alpha.0.zip -d xx
